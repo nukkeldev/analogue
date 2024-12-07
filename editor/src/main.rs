00e6c42f2c82b1ff8e7f85bac6ff4b164fc3f675
nukkeldev <nukkel.dev@gmail.com>
2024-12-06 23:09:54 -0800

    [Editor] Boilerplate

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,5 +1,30 @@
-use analogue::initialize;
+use std::io;
 
-fn main() {
-    initialize();
+use ratatui::{prelude::*, DefaultTerminal};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    let result = App::default().run(&mut terminal);
+    ratatui::restore();
+    result
 }
+
+#[derive(Debug, Default)]
+struct App {}
+
+impl App {
+    pub fn run(&mut self, terminal: &mut DefaultTerminal) -> io::Result<()> {
+        Ok(())
+    }
+}
+
+impl Widget for &App {
+    fn render(self, area: Rect, buf: &mut Buffer)
+    where
+        Self: Sized,
+    {
+    }
+}
+
+#[cfg(test)]
+mod tests {}
